728f166bef120f8e129bbd17011ddec74c2e24b9
Hugh Williams <hughbertwilliams@gmai.com>
2016-12-24 20:49:13 +0000

    Cleaning up the script

--- conv.sh
+++ conv.sh
@@ -1,5 +1,15 @@
-#! /usr/bin/bash
+# Checks for the presence of markdown files
 echo
+md=$(ls -1 *.md 2>/dev/null | wc -l)
+mdown=$(ls -1 *.mdown 2>/dev/null | wc -l)
+mark=$(ls -1 *.markdown 2>/dev/null | wc -l)
+present=$(($md+$mdown+$mark))
+if [ "$present" -eq 0 ]; then
+	echo -e "No markdown files detected!\n"
+	exit
+fi
+
+# Checks for output directory and creates one if missing
 if [ ! -d PDFs ]; then
 	mkdir PDFs
 	echo -e "Output folder created.\n"
@@ -7,14 +17,15 @@ else
 	echo -e "Selecting existing output folder.\n"
 fi
 
+# Finds and prints the total number of files to be compiled
 files=$(ls -p | grep -v / | wc -l)
 unread=$(ls -p PDFs/ | grep -v / | wc -l)
 total=$((files-unread))
 count=0
-
 echo -e "Found $total files to convert\n"
-echo -e "LaTeX engines\n------------\n"
 
+# Asks user for the LaTeX engine to be used
+echo -e "LaTeX engines\n------------\n"
 PS3=$'\n'"Please choose a LaTeX engine: "
 select option in pdflatex lualatex xelatex
 do
@@ -32,9 +43,11 @@ do
 done
 echo
 
-echo -n "Would you like to add any latex typesetting variables(e.g. --variable mainfont:\"Gill Sans\")?: "
+# Allows user to add custom LaTeX variables 
+echo -n "Would you like to add any LaTeX details(e.g. --variable mainfont:\"Gill Sans\")?: "
 read variable
 
+# Compiles documents
 echo -e "\nBeginning conversion...\n"
 
 find . -iname "*.mdown" | while read -r i; do
